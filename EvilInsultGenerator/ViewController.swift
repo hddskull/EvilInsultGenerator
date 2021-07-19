9865c9271c43b9a5945fa14472bf6ac48e52bb34
MaxGladkov <maxdgladkov@gmail.com>
2021-07-19 14:14:25 +0300

    Initial Commit

---
+++ EvilInsultGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EvilInsultGenerator
+//
+//  Created by Max Gladkov on 19.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
